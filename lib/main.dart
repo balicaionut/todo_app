ab35ce0fdca7175cf351d14d426d628454a0db1d
Ionut Balica <balicaionut@gmail.com>
2021-12-22 13:24:09 +0200

    Add new tasks
    functionality to add new tasks

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/app/data/services/storage/services.dart';
+import 'package:todo_app/app/modules/home/binding.dart';
 import 'package:todo_app/app/modules/home/view.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:dotted_border/dotted_border.dart';
 
 void main() async {
   await GetStorage.init();
@@ -15,9 +18,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
+    return GetMaterialApp(
       title: 'ToDo App',
-      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+      initialBinding: HomeBinding(),
+      builder: EasyLoading.init(),
     );
   }
 }
