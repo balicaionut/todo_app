6c0198c95823e7dba06caa21aa6ac9a245902cde
Ionut Balica <balicaionut@gmail.com>
2021-12-23 14:54:59 +0200

    add dialog page completed

--- lib/app/modules/home/widgets/add_dialog.dart
+++ lib/app/modules/home/widgets/add_dialog.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
 import 'package:todo_app/app/core/utils/extensions.dart';
 import 'package:todo_app/app/modules/home/controller.dart';
@@ -21,12 +22,33 @@ class AddDialog extends StatelessWidget {
               IconButton(
                 onPressed: () {
                   Get.back();
+                  homeCtrl.editCtrl.clear();
+                  homeCtrl.changeTask(null);
                 },
                 icon: const Icon(Icons.close),
               ),
               TextButton(
                   style: ButtonStyle(overlayColor: MaterialStateProperty.all(Colors.transparent)),
-                  onPressed: () {},
+                  onPressed: () {
+                    if (homeCtrl.formKey.currentState!.validate()) {
+                      if (homeCtrl.task.value == null) {
+                        EasyLoading.showError('Please select task type');
+                      } else {
+                        var success = homeCtrl.updateTask(
+                          homeCtrl.task.value!,
+                          homeCtrl.editCtrl.text,
+                        );
+                        if (success) {
+                          EasyLoading.showSuccess('ToDo item added');
+                          Get.back();
+                          homeCtrl.changeTask(null);
+                        } else {
+                          EasyLoading.showError('ToDo item already exists');
+                        }
+                        homeCtrl.editCtrl.clear();
+                      }
+                    }
+                  },
                   child: Text(
                     "Done",
                     style: TextStyle(fontSize: 14.0.sp),
@@ -60,7 +82,60 @@ class AddDialog extends StatelessWidget {
               return null;
             },
           ),
-        )
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 5.0.wp, left: 5.0.wp, right: 5.0.wp, bottom: 2.0.wp),
+          child: Text(
+            'Add to',
+            style: TextStyle(
+              fontSize: 14.0.sp,
+              color: Colors.grey,
+            ),
+          ),
+        ),
+        ...homeCtrl.tasks
+            .map((element) => Obx(
+                  () => InkWell(
+                    onTap: () => homeCtrl.changeTask(element),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 3.0.wp,
+                        horizontal: 5.0.wp,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Icon(
+                                  IconData(
+                                    element.icon,
+                                    fontFamily: 'MaterialIcons',
+                                  ),
+                                  color: HexColor.fromHex(element.color)),
+                              SizedBox(
+                                width: 3.0.wp,
+                              ),
+                              Text(
+                                element.title,
+                                style: TextStyle(
+                                  fontSize: 12.0.sp,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                          if (homeCtrl.task.value == element)
+                            const Icon(
+                              Icons.check,
+                              color: Colors.blue,
+                            )
+                        ],
+                      ),
+                    ),
+                  ),
+                ))
+            .toList()
       ]),
     ));
   }
