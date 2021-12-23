e52a54cf255a312b029c10da130162c3690ec4a4
Ionut Balica <balicaionut@gmail.com>
2021-12-23 17:16:22 +0200

    added task card

---
+++ lib/app/modules/detail/view.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:todo_app/app/core/utils/extensions.dart';
+import 'package:todo_app/app/modules/home/controller.dart';
+
+class DetailPage extends StatelessWidget {
+  final homeCtrl = Get.find<HomeController>();
+  DetailPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var task = homeCtrl.task.value!;
+    var color = HexColor.fromHex(task.color);
+    return Scaffold(
+      body: ListView(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(3.0.wp),
+            child: Row(
+              children: [
+                IconButton(
+                  onPressed: () {
+                    Get.back();
+                    homeCtrl.changeTask(null);
+                  },
+                  icon: const Icon(Icons.arrow_back),
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 8.0.wp,
+            ),
+            child: Row(
+              children: [
+                Icon(
+                  IconData(
+                    task.icon,
+                    fontFamily: 'MaterialIcons',
+                  ),
+                  color: color,
+                ),
+                SizedBox(
+                  width: 3.0.wp,
+                ),
+                Text(
+                  task.title,
+                  style: TextStyle(
+                    fontSize: 12.0.sp,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
