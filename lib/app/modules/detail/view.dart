efd76f5a49b9790538ea995acd0a06ac6d50be05
Ionut Balica <balicaionut@gmail.com>
2021-12-24 16:37:56 +0200

    task card completed

--- lib/app/modules/detail/view.dart
+++ lib/app/modules/detail/view.dart
@@ -4,6 +4,7 @@ import 'package:get/get.dart';
 import 'package:step_progress_indicator/step_progress_indicator.dart';
 import 'package:todo_app/app/core/utils/extensions.dart';
 import 'package:todo_app/app/modules/detail/widgets/doing_list.dart';
+import 'package:todo_app/app/modules/detail/widgets/done_list.dart';
 import 'package:todo_app/app/modules/home/controller.dart';
 
 class DetailPage extends StatelessWidget {
@@ -140,6 +141,7 @@ class DetailPage extends StatelessWidget {
               ),
             ),
             DoingList(),
+            DoneList(),
           ],
         ),
       ),
